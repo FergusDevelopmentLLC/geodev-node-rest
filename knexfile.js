69f588b5241697e1634360ae36e7cb7f2d12e633
will.carter@fergusllc.com <will.carter@fergusllc.com>
2017-06-21 17:19:41 -0600

    made knexfile.js smart to know what server it's on for db connections.

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,25 @@
+var os = require('os');
+
+var hostname = os.hostname();
+var type = os.type();
+
+//display the host computer name
+console.log('os.hostname(): ' + os.hostname());
+//display the os type (Linux, Windows, etc.)
+console.log('os.type(): ' + os.type());
+console.log();
+
+//node is not being run locally.
+//port 15432 on the host is forwarded to 5432 on the vm
+var port = 5432;
+if (hostname !== "geodev") { port = 15432; }
+
 module.exports = {
   development: {
     client: 'postgresql',
     connection: {
       host : '127.0.0.1',
-      port: '15432',
+      port: port,
       user : 'geodevdb',
       password : 'admin123',
       database : 'geodevdb'
